bbdf578f9a413defc84b227ac8545b09eb4bce8e
anti-bug <yandma1@gmail.com>
2015-11-28 22:57:33 +0500

    Update hello-world.cpp

--- hello-world.cpp
+++ hello-world.cpp
@@ -4,11 +4,6 @@
 int main(){
 	int x,y,result;
 	cout<<"enter your first value";
-	cin>>"x";
-	cout<<"enter your second value";
-	cin>>"y"
-	result=x+y;
-	cout<<"your result is ="<<result;
 
 	getch();
 
